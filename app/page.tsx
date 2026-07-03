8bfaa2a1f40d0e80ad3600534a3e71c5f2e59798
lonewolfark <dmnstrumde@gmail.com>
2026-07-03 03:56:21 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
-import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import DonationSection from '@/components/DonationSection'
 import ImpactStats from '@/components/ImpactStats'
-import AboutSection from '@/components/AboutSection'
-import Footer from '@/components/Footer'
+import HomeAbout from '@/components/HomeAbout'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-coral-50/30 to-white">
-      <Navbar />
+    <>
       <Hero />
       <DonationSection />
       <ImpactStats />
-      <AboutSection />
-      <Footer />
-    </main>
+      <HomeAbout />
+    </>
   )
 }
